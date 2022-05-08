ad5e05a30874e59719ce45ee1fe0f5ce1ec6ad60
Timo <timotuominen111@hotmail.com>
2022-05-08 07:09:19 +0300

    1

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
+import Header from "./components/Header";
+import Restaurantlist from "./components/Restaurantlist";
 
 function App() {
+  const nimi = "Ravintolan Nimi";
+  const osoite = "Osoite tulee tähän";
+  const tyypppi = "Ravintolan tyyppi";
   return (
     <>
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-        ></link>
-      </head>
-      <h1>Hello world</h1>;
+      <Header />
+      <h1>Hello world</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Restaurantlist />
+        </tbody>
+      </table>
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
---
+++ reactproject/src/components/Restaurantlist.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Restaurantlist() {
+  return (
+    <>
+      <tr>
+        <th>1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </>
+  );
+}
+
+export default Restaurantlist;
