c6a29a57b1afb1227dd970264ac865a9e077617f
Timo <timotuominen111@hotmail.com>
2022-05-09 08:28:24 +0300

    1

--- reactproject/src/components/Restaurantlist.jsx
+++ reactproject/src/components/Restaurantlist.jsx
@@ -6,7 +6,9 @@ function Restaurantlist({ ravintoladata }) {
   if (!ravintoladata || ravintoladata.length === 0) {
     return <p>Ei ravintolatietoja</p>;
   }
-
+  if (!Array.isArray(ravintoladata)) {
+    ravintoladata = [ravintoladata];
+  }
   //console.log(ravintoladata);
   //console.log(ravintoladata.name);
   return (
